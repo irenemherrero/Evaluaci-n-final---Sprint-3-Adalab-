0dd1107408df1610dd0d3d7819b56b35ae02750f
irenemherrero <irenemherrero@gmail.com>
2018-08-09 19:13:41 +0200

    github pages

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   // Llamo a la api para recibir los datos
 
   componentDidMount() {
-    fetch('http://hp-api.herokuapp.com/api/characters')
+    fetch('https://hp-api.herokuapp.com/api/characters')
       .then(response => {
         return response.json();
       })
